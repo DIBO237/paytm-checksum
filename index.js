013d7821a3e5adefac9a23d20a921fc23a6d88f7
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2022-03-09 18:55:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,89 @@
 const https = require('https');
-const express= require('express')
-const app = express()
-/**
-* import checksum generation utility
-* You can get this utility from https://developer.paytm.com/docs/checksum/
-*/
-
-const merchantId = "EcWTAk01170313958935";
-const merchantKey = "Nm2SVBVbzvKyAWpI";
-const referenceId = "pinamsa234d42f21";
-
+const express = require('express');
+const app = express();
 const PaytmChecksum = require('paytmchecksum');
+/**
+ * import checksum generation utility
+ * You can get this utility from https://developer.paytm.com/docs/checksum/
+ */
+
+const merchantId = 'EcWTAk01170313958935';
+const merchantKey = 'Nm2SVBVbzvKyAWpI';
+const referenceId = 'pinamsa234d42f21';
 
 var paytmParams = {};
 
 paytmParams.body = {
-    "mid": merchantId,
-    "referenceId": referenceId,
-    "cardPreAuthType": "STANDARD_AUTH",
-    "preAuthBlockSeconds": "12321"
+  mid: merchantId,
+  referenceId: referenceId,
+  cardPreAuthType: 'STANDARD_AUTH',
+  preAuthBlockSeconds: '12321',
 };
 
 /**
-* Generate checksum by parameters we have in body
-* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
-*/
-PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), merchantKey).then(function (checksum) {
-    paytmParams.head = {
-        "tokenType": "CHECKSUM",
-        "token": checksum
-    };
+ * Generate checksum by parameters we have in body
+ * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+ */
 
-    var post_data = JSON.stringify(paytmParams);
+app.get('/', (req, res) => res.send('paytm Server'));
 
-    var options = {
+app.post('/createOrder', async (req, res) => {
+  try {
+    // const order = await PaytmChecksum.generateSignature(
+    //   { mid: merchantId, orderId: req.body.orderId },
+    //   merchantKey
+    // ).then(function (checksum) {
+    //   paytmParams.head = {
+    //     tokenType: 'CHECKSUM',
+    //     token: checksum,
+    //   };
 
-        /* for Staging */
-        hostname: 'securegw-stage.paytm.in',
+    //   var post_data = JSON.stringify(paytmParams);
 
-        /* for Production */
-        // hostname: 'securegw.paytm.in',
+    //   var options = {
+    //     /* for Staging */
+    //     hostname: 'securegw-stage.paytm.in',
 
-        port: 443,
-        path: '/theia/api/v1/token/create?mid=' + merchantId + '&referenceId=' + referenceId,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': post_data.length
-        }
-    };
+    //     /* for Production */
+    //     // hostname: 'securegw.paytm.in',
 
-    var response = "";
-    var post_req = https.request(options, function (post_res) {
-        post_res.on('data', function (chunk) {
-            response += chunk;
-        });
+    //     port: 443,
+    //     path:
+    //       '/theia/api/v1/token/create?mid=' +
+    //       merchantId +
+    //       '&referenceId=' +
+    //       referenceId,
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Content-Length': post_data.length,
+    //     },
+    //   };
 
-        post_res.on('end', function () {
-            console.log('Response: ', response);
-        });
-    });
+    //   var response = '';
+    //   var post_req = https.request(options, function (post_res) {
+    //     post_res.on('data', function (chunk) {
+    //       response += chunk;
+    //     });
 
-    post_req.write(post_data);
-    post_req.end();
+    //     post_res.on('end', function () {
+    //       console.log('Response: ', response);
+    //       return response;
+    //     });
+    //   });
+
+    //   post_req.write(post_data);
+    //   post_req.end();
+    // });
+    const order = {}
+    res.send({success:true,order});
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-const port = (process.env.PORT || 3000)
+const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
-     console.log("server is running")
-})
+app.listen(port, () => {
+  console.log('server is running');
+});
